9614143ac8e48a4172304a0b4bacab7d3fa2becd
Igor Santos <falaigors@gmail.com>
2022-07-05 00:20:26 -0300

    application styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { PlusCircle, Trash } from "phosphor-react";
 import logoUrl from "./images/Logo.svg";
 import * as S from "./styles/App";
 
@@ -7,6 +8,47 @@ export default function App() {
       <S.Header>
         <img src={logoUrl} alt="logo" />
       </S.Header>
+      <S.Container>
+        <S.Form>
+          <S.Input
+            type="text"
+            value=""
+            placeholder="Adicione uma nova tarefa"
+          />
+          <S.Button type="button">
+            Criar <PlusCircle weight="bold" style={{ marginLeft: "4px" }} />
+          </S.Button>
+        </S.Form>
+      </S.Container>
+
+      <S.Container>
+        <S.TasksInfo>
+          <S.TasksInfoCreate>
+            <h3>Tarefas criadas</h3>
+            <span>0</span>
+          </S.TasksInfoCreate>
+
+          <S.TasksInfoComplete>
+            <h3>Concluídas</h3>
+            <span>0</span>
+          </S.TasksInfoComplete>
+        </S.TasksInfo>
+      </S.Container>
+
+      <S.Container>
+        <S.TasksList>
+          <S.TaskItem>
+            <input type="checkbox" />
+            <p>
+              Integer urna interdum massa libero auctor neque turpis turpis
+              semper. Duis vel sed fames integer.
+            </p>
+            <button>
+              <Trash />
+            </button>
+          </S.TaskItem>
+        </S.TasksList>
+      </S.Container>
     </>
   );
 }
--- src/styles/App.ts
+++ src/styles/App.ts
@@ -3,4 +3,124 @@ import styled from "styled-components";
 export const Header = styled.header`
   background-color: var(--gray-700);
   padding: 80px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Container = styled.section`
+  max-width: 768px;
+  margin: 0 auto;
+`;
+
+export const Form = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 8px;
+  margin-top: -30px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  color: var(--gray-100);
+  padding: 16px;
+  border-radius: 8px;
+  border-color: var(--gray-700);
+  background-color: var(--gray-500);
+
+  :focus {
+    color: var(--gray-100);
+    outline-color: var(--purple-dark);
+  }
+
+  :placeholder {
+    color: var(--gray-300);
+  }
+`;
+
+export const Button = styled.button`
+  padding: 16px;
+  border: 0;
+  outline: 1;
+  border-radius: 8px;
+  display: inline-flex;
+  align-items: center;
+  font-weight: 500;
+  color: var(--gray-100);
+  outline-color: var(--blue);
+  background-color: var(--blue-dark);
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: var(--blue);
+  }
+`;
+
+export const TasksInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 64px;
+
+  span {
+    font-size: 1rem;
+    padding: 2px 8px;
+    font-weight: bold;
+    border-radius: 99px;
+    background-color: var(--gray-400);
+    color: var(--gray-100);
+  }
+`;
+
+export const TasksInfoCreate = styled.div`
+  display: flex;
+  color: var(--blue);
+  gap: 8px;
+`;
+
+export const TasksInfoComplete = styled(TasksInfoCreate)`
+  color: var(--purple);
+`;
+
+export const TasksList = styled.div`
+  margin-top: 24px;
+  width: 100%;
+`;
+
+export const TaskItem = styled.div`
+  display: flex;
+  padding: 16px;
+  justify-content: space-between;
+  background-color: var(--gray-500);
+  border-radius: 8px;
+  margin: 10px 0;
+
+  input[type="checkbox"] {
+    height: 15px;
+    width: 15px;
+    background-color: var(--gray-500);
+  }
+
+  p {
+    flex: 2;
+    padding: 0 12px;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 4px;
+    height: 25px;
+    width: 25px;
+    border: 0;
+    padding: 5px;
+    color: var(--gray-300);
+    background-color: var(--gray-500);
+    transition: all 0.2s ease-out;
+
+    :hover {
+      color: var(--danger);
+      background-color: var(--gray-400);
+    }
+  }
 `;
