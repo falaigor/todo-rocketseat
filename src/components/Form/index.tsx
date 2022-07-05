e40f8227780ad401309df23c8b005dc08cff9abd
Igor Santos <falaigors@gmail.com>
2022-07-05 00:28:03 -0300

    create a function input form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { PlusCircle } from "phosphor-react";
 import * as S from "./styles";
 
 export const Form = () => {
+  const [value, setValue] = useState("");
+
   return (
     <S.Form>
-      <S.Input type="text" value="" placeholder="Adicione uma nova tarefa" />
+      <S.Input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Adicione uma nova tarefa"
+      />
       <S.Button type="button">
         Criar <PlusCircle weight="bold" style={{ marginLeft: "4px" }} />
       </S.Button>
